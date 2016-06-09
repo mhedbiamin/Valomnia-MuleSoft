9810325e04fc7a7dc802eebc6b31a2044f97fcf4
mhedbiamin <mhedbim@yahoo.fr>
2016-06-09 15:16:33 +0100

    update

--- src/main/java/org/mule/modules/valomnia/entities/Item.java
+++ src/main/java/org/mule/modules/valomnia/entities/Item.java
@@ -8,6 +8,8 @@ package org.mule.modules.valomnia.entities;
 
 public class Item {
 
+	
+
 	private Integer id;
 	private String reference;
 	private String organization;
@@ -254,4 +256,36 @@ public class Item {
 		this.displayOrder = displayOrder;
 	}
 
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (categoryReference == null) {
+			if (other.categoryReference != null)
+				return false;
+		} else if (!categoryReference.equals(other.categoryReference))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (reference == null) {
+			if (other.reference != null)
+				return false;
+		} else if (!reference.equals(other.reference))
+			return false;
+		return true;
+	}
 }
