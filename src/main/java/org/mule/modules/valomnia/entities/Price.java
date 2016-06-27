979b1a9123f5d2d3e17bde5c229993e1c275f3ef
mhedbiamin <mhedbim@yahoo.fr>
2016-06-27 10:44:27 +0100

    update

--- src/main/java/org/mule/modules/valomnia/entities/Price.java
+++ src/main/java/org/mule/modules/valomnia/entities/Price.java
@@ -1,120 +1,206 @@
 /**
  * (c) 2003-2016 MuleSoft, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1, a copy of which has been included with this distribution in the LICENSE.md file.
  */
-/**
- *
- */
+
 package org.mule.modules.valomnia.entities;
 
+
+
+
 public class Price {
-	private String id;
 
-	private String organization;
+private Integer id;
+private String priceListReference;
+private String itemReference;
+private String unitReference;
+private Integer value;
+private Object unitPrice;
+private Object marginRate;
+private String organization;
+private String dateCreated;
+private String lastUpdated;
 
-	private String unitReference;
 
-	private String priceListReference;
+/**
+* 
+* @return
+* The id
+*/
+public Integer getId() {
+return id;
+}
 
-	private String itemReference;
+/**
+* 
+* @param id
+* The id
+*/
+public void setId(Integer id) {
+this.id = id;
+}
 
-	private String value;
+/**
+* 
+* @return
+* The priceListReference
+*/
+public String getPriceListReference() {
+return priceListReference;
+}
 
-	private String lastUpdated;
+/**
+* 
+* @param priceListReference
+* The priceListReference
+*/
+public void setPriceListReference(String priceListReference) {
+this.priceListReference = priceListReference;
+}
 
-	private String dateCreated;
+/**
+* 
+* @return
+* The itemReference
+*/
+public String getItemReference() {
+return itemReference;
+}
 
-	private String unitPrice;
+/**
+* 
+* @param itemReference
+* The itemReference
+*/
+public void setItemReference(String itemReference) {
+this.itemReference = itemReference;
+}
 
-	private String marginRate;
+/**
+* 
+* @return
+* The unitReference
+*/
+public String getUnitReference() {
+return unitReference;
+}
 
-	public String getId() {
-		return id;
-	}
+/**
+* 
+* @param unitReference
+* The unitReference
+*/
+public void setUnitReference(String unitReference) {
+this.unitReference = unitReference;
+}
 
-	public void setId(String id) {
-		this.id = id;
-	}
+/**
+* 
+* @return
+* The value
+*/
+public Integer getValue() {
+return value;
+}
 
-	public String getOrganization() {
-		return organization;
-	}
+/**
+* 
+* @param value
+* The value
+*/
+public void setValue(Integer value) {
+this.value = value;
+}
 
-	public void setOrganization(String organization) {
-		this.organization = organization;
-	}
+/**
+* 
+* @return
+* The unitPrice
+*/
+public Object getUnitPrice() {
+return unitPrice;
+}
 
-	public String getUnitReference() {
-		return unitReference;
-	}
+/**
+* 
+* @param unitPrice
+* The unitPrice
+*/
+public void setUnitPrice(Object unitPrice) {
+this.unitPrice = unitPrice;
+}
 
-	public void setUnitReference(String unitReference) {
-		this.unitReference = unitReference;
-	}
+/**
+* 
+* @return
+* The marginRate
+*/
+public Object getMarginRate() {
+return marginRate;
+}
 
-	public String getPriceListReference() {
-		return priceListReference;
-	}
+/**
+* 
+* @param marginRate
+* The marginRate
+*/
+public void setMarginRate(Object marginRate) {
+this.marginRate = marginRate;
+}
 
-	public void setPriceListReference(String priceListReference) {
-		this.priceListReference = priceListReference;
-	}
+/**
+* 
+* @return
+* The organization
+*/
+public String getOrganization() {
+return organization;
+}
 
-	public String getItemReference() {
-		return itemReference;
-	}
+/**
+* 
+* @param organization
+* The organization
+*/
+public void setOrganization(String organization) {
+this.organization = organization;
+}
 
-	public void setItemReference(String itemReference) {
-		this.itemReference = itemReference;
-	}
+/**
+* 
+* @return
+* The dateCreated
+*/
+public String getDateCreated() {
+return dateCreated;
+}
 
-	public String getValue() {
-		return value;
-	}
+/**
+* 
+* @param dateCreated
+* The dateCreated
+*/
+public void setDateCreated(String dateCreated) {
+this.dateCreated = dateCreated;
+}
 
-	public void setValue(String value) {
-		this.value = value;
-	}
+/**
+* 
+* @return
+* The lastUpdated
+*/
+public String getLastUpdated() {
+return lastUpdated;
+}
 
-	public String getLastUpdated() {
-		return lastUpdated;
-	}
+/**
+* 
+* @param lastUpdated
+* The lastUpdated
+*/
+public void setLastUpdated(String lastUpdated) {
+this.lastUpdated = lastUpdated;
+}
 
-	public void setLastUpdated(String lastUpdated) {
-		this.lastUpdated = lastUpdated;
-	}
 
-	public String getDateCreated() {
-		return dateCreated;
-	}
 
-	public void setDateCreated(String dateCreated) {
-		this.dateCreated = dateCreated;
-	}
-
-	public String getUnitPrice() {
-		return unitPrice;
-	}
-
-	public void setUnitPrice(String unitPrice) {
-		this.unitPrice = unitPrice;
-	}
-
-	public String getMarginRate() {
-		return marginRate;
-	}
-
-	public void setMarginRate(String marginRate) {
-		this.marginRate = marginRate;
-	}
-
-	@Override
-	public String toString() {
-		return "ClassPojo [id = " + id + ", organization = " + organization
-				+ ", unitReference = " + unitReference
-				+ ", priceListReference = " + priceListReference
-				+ ", itemReference = " + itemReference + ", value = " + value
-				+ ", lastUpdated = " + lastUpdated + ", dateCreated = "
-				+ dateCreated + ", unitPrice = " + unitPrice
-				+ ", marginRate = " + marginRate + "]";
-	}
 }
