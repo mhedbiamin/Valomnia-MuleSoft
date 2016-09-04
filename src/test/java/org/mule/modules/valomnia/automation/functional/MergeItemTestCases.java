3b36f962856c45ae1605da51275189dab9b257b6
mhedbiamin <mhedbim@yahoo.fr>
2016-09-04 23:12:32 +0100

    update

--- src/test/java/org/mule/modules/valomnia/automation/functional/MergeItemTestCases.java
+++ src/test/java/org/mule/modules/valomnia/automation/functional/MergeItemTestCases.java
@@ -5,8 +5,6 @@ package org.mule.modules.valomnia.automation.functional;
 
 import static org.junit.Assert.*;
 
-
-
 import java.util.List;
 
 import org.junit.Test;
@@ -14,86 +12,105 @@ import org.mule.modules.valomnia.ValomniaConnector;
 import org.mule.modules.valomnia.entities.Item;
 import org.mule.tools.devkit.ctf.junit.AbstractTestCase;
 
-
-
 public class MergeItemTestCases extends AbstractTestCase<ValomniaConnector> {
 
-    public MergeItemTestCases() {
-        super(ValomniaConnector.class);
-    }
+	public MergeItemTestCases() {
+		super(ValomniaConnector.class);
+	}
 
-    
-    
-    @Test
-    public void verify() {
-        java.lang.String expected1 = "Success Updated";
-        java.lang.String expected2 = "Success created";
-        Item obj = new Item();
+	@Test
+	public void verify() {
+		java.lang.String expected1 = "Success Updated";
+		java.lang.String expected2 = "Success created";
+		Item obj = new Item();
 
-        boolean exist = false;
+		boolean exist = false;
 
-        List<Item> list = null;
-        try {
-            list = getConnector().findItems();
-        } catch (Exception e) {
+		List<Item> list = null;
+		try {
+			list = getConnector().findItems();
+		} catch (Exception e) {
 
-            e.printStackTrace();
-        }
-        
+			e.printStackTrace();
+		}
 
-        for (Item item : list)
-        {
-            if (item.getReference()
-                    .equals("ref test Item"))
-                exist = true;
-        }
-        obj.setName("test Item");
-        obj.setOrganization("test");
-        obj.setReference("ref test Item");
-        obj.setCategoryReference("ref test ItemCategory");
+		for (Item item : list) {
+			if (item.getReference().equals("ref test Item"))
+				exist = true;
+		}
+		obj.setName("test Item");
+		obj.setReference("ref test Item");
+		obj.setCategoryReference("ref test ItemCategory");
 
-        obj.setDescription("TEST DESCRIPTION");
+		obj.setDescription("TEST DESCRIPTION");
 
-        if (!exist)
-            assertEquals(getConnector().mergeItem(obj), expected2);
-        else
-            assertEquals(getConnector().mergeItem(obj), expected1);
-    }
-    
-    
-    
-    
-    @Test
-    public void itemReferenceMissingTest() {
-        java.lang.String expected = "Reference missing";
-        
-        Item obj = new Item();   
-        obj.setName("test Item");
-       
-        obj.setCategoryReference("ref test ItemCategory");
+		if (!exist)
+			assertEquals(getConnector().mergeItem(obj), expected2);
+		else
+			assertEquals(getConnector().mergeItem(obj), expected1);
+	}
 
-        obj.setDescription("TEST DESCRIPTION");
+	@Test
+	public void itemReferenceMissingTest() {
+		java.lang.String expected = "Reference missing";
 
-        String apiResponse=getConnector().mergeItem(obj);
-            assertTrue(apiResponse.contains(expected ));
-    }
-    
-    
-    
-    
-    @Test
-    public void verifyItemSaved() {
-    	
-    	List<Item> list = null;
-    	boolean   exist=false;
-        
-            list = getConnector().findItems();
-        
-        for (Item  item:list)
-        { if ( item.getReference().equals("ref test Item"))
-            exist=true;
-        }
-    	assertTrue(exist);
-    }
+		Item obj = new Item();
+		obj.setName("test Item");
 
+		obj.setCategoryReference("ref test ItemCategory");
+
+		obj.setDescription("TEST DESCRIPTION");
+
+		String apiResponse = getConnector().mergeItem(obj);
+		assertTrue(apiResponse.contains(expected));
+	}
+
+	@Test
+	public void verifyItemSaved() {
+
+		List<Item> list = null;
+		boolean exist = false;
+
+		list = getConnector().findItems();
+
+		for (Item item : list) {
+			if (item.getReference().equals("ref test Item"))
+				exist = true;
+		}
+		assertTrue(exist);
+	}
+
+	@Test
+	public void missingItemNameTest() {
+
+		java.lang.String expected = "Name required";
+
+		Item obj = new Item();
+		obj.setReference("ref test Item");
+
+		obj.setCategoryReference("ref test ItemCategory");
+
+		obj.setDescription("TEST DESCRIPTION");
+
+		String apiResponse = getConnector().mergeItem(obj);
+		assertTrue(apiResponse.contains(expected));
+	}
+
+	@Test
+	public void updateItemTest() {
+
+		java.lang.String expected = "OK";
+
+		Item obj = new Item();
+		obj.setName("test Item");
+		obj.setReference("ref test Item");
+		obj.setCategoryReference("ref test ItemCategory");
+
+		
+
+		
+
+		String apiResponse = getConnector().mergeItem(obj);
+		assertTrue(apiResponse.contains(expected));
+	}
 }
