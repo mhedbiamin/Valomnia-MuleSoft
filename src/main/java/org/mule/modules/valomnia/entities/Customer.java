ae1c445dcfc8be5c635f74ca6cf277ed2322607a
mhedbiamin <mhedbim@yahoo.fr>
2016-09-12 13:39:43 +0100

    update

--- src/main/java/org/mule/modules/valomnia/entities/Customer.java
+++ src/main/java/org/mule/modules/valomnia/entities/Customer.java
@@ -288,37 +288,6 @@ public class Customer {
 		this.mobile = mobile;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Customer other = (Customer) obj;
-		if (address == null) {
-			if (other.address != null)
-				return false;
-		} else if (!address.equals(other.address))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (reference == null) {
-			if (other.reference != null)
-				return false;
-		} else if (!reference.equals(other.reference))
-			return false;
-		return true;
-	}
+	
 
 }
