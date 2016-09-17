f5d32601450e99964d5eb0f07da980e257091ca5
mhedbiamin <mhedbim@yahoo.fr>
2016-09-17 14:42:25 +0100

    update

--- src/test/java/org/mule/modules/valomnia/automation/unit/WareHouseStockTest.java
+++ src/test/java/org/mule/modules/valomnia/automation/unit/WareHouseStockTest.java
@@ -1,7 +1,7 @@
 /**
  * (c) 2003-2016 MuleSoft, Inc. The software in this package is Licensed under the Apache License, Version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
- * adaz  Other license terms have been included with this distribution in the LICENSE.md file.
+ * Other license terms have been included with this distribution in the LICENSE.md file.
  */
 package org.mule.modules.valomnia.automation.unit;
 
