ab46e6a37d3a2c7f66702ed31a0cc0fb0d06af51
mhedbiamin <mhedbim@yahoo.fr>
2016-09-17 14:12:07 +0100

    update

--- src/main/java/org/mule/modules/valomnia/entities/Authority.java
+++ src/main/java/org/mule/modules/valomnia/entities/Authority.java
@@ -3,12 +3,7 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
  * Other license terms have been included with this distribution in the LICENSE.md file.
  */
-/**
- * (c) 2003-2016 MuleSoft, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1, a copy of which has been included with this distribution in the LICENSE.md file.
- */
-/**
- *
- */
+
 package org.mule.modules.valomnia.entities;
 
 public class Authority {
